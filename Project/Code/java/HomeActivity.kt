32f5d1e1b9b09b1deaed2d832d3a9758cd043a88
rrm15 <220701283@rajalakshmi.edu.in>
2025-05-10 18:59:57 +0530

    Pushed

---
+++ Project/Code/java/HomeActivity.kt
@@ -0,0 +1,43 @@
+package com.example.ezbill
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ezbill.utils.FirebaseUtils
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fabAddToCart)
+        fab.setOnClickListener {
+            startActivity(Intent(this, CartActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_add_product -> {
+                startActivity(Intent(this, AddProductActivity::class.java))
+                true
+            }
+            R.id.menu_logout -> {
+                FirebaseUtils.auth.signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
